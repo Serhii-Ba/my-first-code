574c5df34d5631b6c9dad742c79e7fffebaf56cd
Serhii-Ba <tatko.tatko8@gmail.com>
2022-01-11 00:03:54 +0200

    новый Урок № 25 Знакомство с JS (калькулятор с набором кнопок, каждая кнопка отвечает за свою операцию и ввод числа)

---
+++ Less 25/old version with evel/index.js
@@ -0,0 +1,25 @@
+ function clearEnterValue() {
+     let enterElement = document.getElementById('enter');
+     enterElement.value = '';
+ }
+ // вар 1.
+ // function enterNumber(v) {
+ //    document.form.textview.value = document.form.textview.value + v;
+ // }
+
+ // вар 2.
+ function enterNumber(v) {
+     document.getElementById("enter").value += v;
+ }
+
+ function enterBack() {
+    let back = document.form.textview.value;
+    document.form.textview.value = back.substring(0,back.length-1);
+ }
+
+ //если бы можно было использовать eval, то...
+ function enterAction() {
+     var num1 = document.getElementById("enter").value;
+     var num2 = eval(num1);
+     document.getElementById("enter").value = num2;
+ }
